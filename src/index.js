00097289b89c93b0448e66b7a9cda6d2b7eb5c88
Harshith M <mharshith200@gmail.com>
2026-03-08 10:39:43 +0530

    Render config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import dotenv from 'dotenv'
-
+import dotenv from "dotenv";
 dotenv.config();
 
-console.log(process.env.user)
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Project Camp Backend Live 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
