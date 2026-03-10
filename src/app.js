359830d815599c98dcfa8ecea129b3c2bc48716e
Harshith M <mharshith200@gmail.com>
2026-03-10 21:18:42 +0530

    Add rate limiter & helmet to project

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
-const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+const app = express();
+
 app.get("/", (req, res) => {
   res.send("Project Camp Backend Live 🚀");
 });
 
+app.set("trust proxy", 1);
+
+// middleware
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-// cors configuration
+
+// CORS
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:3000",
@@ -22,13 +30,26 @@ app.use(
   }),
 );
 
-// importing routes
+// rate limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    success: false,
+    message: "Too many requests from this IP, please try again later.",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(limiter);
+app.use(helmet());
+
+// routes
 import healthCheckRoutes from "./routes/healthcheck.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 
-// using routes
 app.use("/api/v1/auth", authRoutes);
-
 app.use("/api/v1/healthcheck", healthCheckRoutes);
 
 export default app;
